e590b2398e5317b4776d1490dfd6b06e464f0d20
DairoGil <dairogb723@gmail.com>
2022-03-12 15:52:47 -0500

    Project initialized

---
+++ src/main/java/com/springpractice/roulette/rouletteapispring/RouletteApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.springpractice.roulette.rouletteapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springpractice/roulette/rouletteapispring/RouletteApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.roulette.rouletteapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
